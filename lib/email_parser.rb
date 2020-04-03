eaaa476f6f39a4908329da0f708c1ac4dc753724
Dakota Wicker <dwicker1996@gmail.com>
2020-04-03 17:25:16 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -8,7 +8,7 @@ class EmailParser
   end
 
   def parse
-    email_array = @email.split(/[, ]/).uniq
+    email_array = @email.split(/[,]/).uniq
     email_array.reject! {|element| element.empty?}
     email_array
   end
